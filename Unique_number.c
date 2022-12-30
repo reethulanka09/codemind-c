a23990c9fe4e6605e4e602ca7e19589e046a824d
reethulanka09 <119839740+reethulanka09@users.noreply.github.com>
2022-12-30 21:35:35 +0530

    #include<stdio.h> int main() {     int n,a,b,c,k=0,m,i=0;     scanf("%d",&n);     m=n;     for(a=m;a>0;a=a/10)     {         i++;         b=a%10;         for(n=m;n>0;n=n/10)         {             c=n%10;if(b==c)             {                 k++;
                }
                
            }
            
        }
        if(k==i)
        {
            printf("Unique Number");
            
        }
        else {printf("Not Unique Number");
            
        }
        
    }

---
+++ Unique_number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,k=0,m,i=0;
+    scanf("%d",&n);
+    m=n;
+    for(a=m;a>0;a=a/10)
+    {
+        i++;
+        b=a%10;
+        for(n=m;n>0;n=n/10)
+        {
+            c=n%10;if(b==c)
+            {
+                k++;
+                
+            }
+            
+        }
+        
+    }
+    if(k==i)
+    {
+        printf("Unique Number");
+        
+    }
+    else {printf("Not Unique Number");
+        
+    }
+    
+}
