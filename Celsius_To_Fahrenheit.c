3b847007269aecd7ffc80f6b7f86a29952491d78
reethulanka09 <119839740+reethulanka09@users.noreply.github.com>
2023-01-01 14:57:45 +0530

    #include <stdio.h> int main() { 	int a[1],i; 	float c; 	for( int i=0;i<1;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<1;i++) 	{ 	    c=(1.8*a[0]+32); 	} 	printf("%0.2f",c); }

---
+++ Celsius_To_Fahrenheit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a[1],i;
+	float c;
+	for( int i=0;i<1;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<1;i++)
+	{
+	    c=(1.8*a[0]+32);
+	}
+	printf("%0.2f",c);
+}
