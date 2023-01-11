4fa0cdae41cd2afe23ab3577ab0cd478ff7dcda3
reethulanka09 <119839740+reethulanka09@users.noreply.github.com>
2023-01-11 13:09:52 +0530

    #include<stdio.h> float inc(int h) {     float i;     i=2.54*h;     return i; } int main() {     int h;     scanf("%d",&h);     float op=inc(h);     printf("%0.2f",op); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+float inc(int h)
+{
+    float i;
+    i=2.54*h;
+    return i;
+}
 int main()
 {
     int h;
     scanf("%d",&h);
-    float i;
-    i=2.54*h;
-    printf("%0.2f",i);
+    float op=inc(h);
+    printf("%0.2f",op);
 }
