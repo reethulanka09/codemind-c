206a65ce42c4d29e0a63e7ec46ebb614b785dcc4
reethulanka09 <119839740+reethulanka09@users.noreply.github.com>
2023-01-12 12:24:16 +0530

    #include<stdio.h> int a(int b1,int b2,int x) {     int avg;     avg=3*x-(b1+b2);     return avg; } int main() {     int b1,b2,x;     scanf("%d%d%d",&b1,&b2,&x);     int b=a(b1,b2,x);     printf("%d",b); }

--- Average_weight.c
+++ Average_weight.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
+int a(int b1,int b2,int x)
+{
+    int avg;
+    avg=3*x-(b1+b2);
+    return avg;
+}
 int main()
 {
-    int x,b1,b2;
-    scanf("%d%d%d",&x,&b1,&b2);
-    int w;
-    w=3*x-(b1+b2);
-    printf("%d",w);
+    int b1,b2,x;
+    scanf("%d%d%d",&b1,&b2,&x);
+    int b=a(b1,b2,x);
+    printf("%d",b);
 }
