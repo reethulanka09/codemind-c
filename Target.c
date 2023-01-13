47b3b2d1b215ef4e9e1f16a32fa0b5ea8606b39e
reethulanka09 <119839740+reethulanka09@users.noreply.github.com>
2023-01-13 21:46:05 +0530

    #include<stdio.h> int main() {     int p1P1,p2P2,p3P3,p4P4;     scanf("%d%d%d%d",&p1P1,&p2P2,&p3P3,&p4P4);     if(p1P1>=10 && p2P2>=10 && p3P3>=10 && p4P4>=10)     printf("YES");     else     printf("NO"); }

--- Target.c
+++ Target.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c,d;
-    scanf("%d%d%d%d",&a,&b,&c,&d);
-    if(a>=10 && b>=10 && c>=10 && d>=10)
+    int p1P1,p2P2,p3P3,p4P4;
+    scanf("%d%d%d%d",&p1P1,&p2P2,&p3P3,&p4P4);
+    if(p1P1>=10 && p2P2>=10 && p3P3>=10 && p4P4>=10)
     printf("YES");
-    else 
+    else
     printf("NO");
 }
